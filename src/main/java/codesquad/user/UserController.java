6b169fc415a461b1b0eea1b3a8d9dbed82b5df06
javajigi <javajigi@gmail.com>
2018-11-14 19:38:32 +0900

    step1

---
+++ src/main/java/codesquad/user/UserController.java
@@ -0,0 +1,28 @@
+package codesquad.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+    private List<User> users = new ArrayList<>();
+
+    @PostMapping("/user/create")
+    public String create(User user, Model model) {
+        System.out.println("execute create!!");
+        System.out.println("user : " + user);
+        users.add(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String list(Model model) {
+        model.addAttribute("users", users);
+        return "user/list";
+    }
+}
