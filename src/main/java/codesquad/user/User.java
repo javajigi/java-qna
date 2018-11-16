6792500bb270ce1eb45f7f866c9fd48ae57f5f88
javajigi <javajigi@gmail.com>
2018-11-16 14:23:16 +0900

    step2

--- src/main/java/codesquad/user/User.java
+++ src/main/java/codesquad/user/User.java
@@ -1,6 +1,16 @@
 package codesquad.user;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
     private String userId;
     private String password;
     private String name;
--- src/main/java/codesquad/user/UserController.java
+++ src/main/java/codesquad/user/UserController.java
@@ -1,28 +1,33 @@
 package codesquad.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-    private List<User> users = new ArrayList<>();
+    @Autowired
+    private UserRepository userRepository;
 
-    @PostMapping("/user/create")
+    @PostMapping("")
     public String create(User user, Model model) {
         System.out.println("execute create!!");
         System.out.println("user : " + user);
-        users.add(user);
-        return "redirect:/users";
+        userRepository.save(user);
+        return list(model);
     }
 
-    @GetMapping("/users")
+    @GetMapping("")
     public String list(Model model) {
-        model.addAttribute("users", users);
+        model.addAttribute("users", userRepository.findAll());
         return "user/list";
     }
 }
